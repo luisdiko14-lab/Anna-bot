ae727324e82f3261ae04930b7c5da32bec1f5fae
luisdiko14-lab <luisdiko14@gmail.com>
2025-11-23 16:44:36 +0100

    Create bot-info.bat to generate Anna Bot info file
    This batch file creates a text file with information about Anna Bot, including its features and creator details.

---
+++ bot-info.bat
@@ -0,0 +1,17 @@
+@echo off
+:: Set output file path
+set "outfile=AnnaBotInfo.txt"
+
+:: Create the file with content
+echo 🌟 What’s Anna Bot? 🌟 > "%outfile%"
+echo. >> "%outfile%"
+echo Anna the Guardian 🛡️ will help you moderate your own server, create reaction roles, and more. 🤖✨ >> "%outfile%"
+echo. >> "%outfile%"
+echo Created by: @zelda_life >> "%outfile%"
+echo Contact: @zelda_life >> "%outfile%"
+echo Support Server: https://discord.gg/RJPjWhfRFE >> "%outfile%"
+echo Coded by: @luisthegoat7301 >> "%outfile%"
+
+:: Notify user
+echo File "%outfile%" has been created successfully!
+pause
